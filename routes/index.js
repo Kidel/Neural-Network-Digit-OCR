384225bf9cbb5089a8429a3b1bc8648e1f0780c2
Kidel <tano.design@gmail.com>
2016-05-02 09:31:32 +0200

    working now

--- routes/index.js
+++ routes/index.js
@@ -16,6 +16,24 @@ var Neuron = synaptic.Neuron,
 var net = new Architect.Perceptron(1024,30,10);
 var trainer = null;
 
+function indexOfMax(arr) {
+    if (arr.length === 0) {
+        return -1;
+    }
+
+    var max = arr[0];
+    var maxIndex = 0;
+
+    for (var i = 1; i < arr.length; i++) {
+        if (arr[i] > max) {
+            maxIndex = i;
+            max = arr[i];
+        }
+    }
+
+    return maxIndex;
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('index', { title: 'Index', text: 'Go to /store to transfer data to the database, then go to /train to start training, and then finally you can use the canvas down here to test the neural network' });
@@ -37,13 +55,17 @@ router.get('/train', function(req, res, next) {
         if(err) return res.json(500, { message: 'Error getting data' });
 		//console.log(trainingSet[0]);
 		trainer = new Trainer(net,{
-							//rate: .1,
-							iterations: 100,
-							error: 0.0000001,
-							shuffle: true
+							rate: 0.03,
+							iterations: 10,
+							error: 0.000001,
+							shuffle: true,
+							log: true,
+							cost: Trainer.cost.CROSS_ENTROPY
 						});
-        var ocr = trainer.train(trainingSet) 
-		console.log(ocr);
+		for(var i=0; i<10; i++){
+			var ocr = trainer.train(trainingSet);
+			console.log(ocr);
+		}
 		//console.log(trainer);
         res.render('index', { title: 'Index', text: "Training done" });
     });
@@ -53,7 +75,7 @@ router.get('/test', function(req, res, next) {
     testSetModel.find({}).select({input:1, output:1, _id:0}).exec(function(err, testSet){
         if(err) return res.json(500, { message: 'Error getting data' });
 		try {
-			res.render('index', { title: 'Index', text: net.activate(testSet[200].input) + " " +  testSet[200].output});
+			res.render('index', { title: 'Index', text: indexOfMax(net.activate(testSet[200].input)) + " " + indexOfMax(testSet[200].output)});
 		}
 		catch (e) {
 			res.render('index', { title: 'Index', text: "You need to train the network first" });
@@ -69,7 +91,7 @@ router.post('/testCharacter', function(req, res, next) {
 			var result = net.activate(letter);
 			//console.log(letter[0])
 			//console.log(result)
-			res.json(result);
+			res.json(indexOfMax(result));
 		}
 	}
 	catch (e) {
